e58c9339c8293e1ae97553c260b39fc450042d29
Ilya Kramarenko <il.kramarenko1@gmail.com>
2023-02-16 17:36:03 +0200

    uniqueized code

--- vanillaQuiz/script.js
+++ vanillaQuiz/script.js
@@ -59,12 +59,12 @@ const data = [
 	},
 ];
 
-const gameScreen = document.querySelector(".game");
-const resultScreen = document.querySelector(".result");
-const question = document.querySelector(".question");
-const answersContainer = document.querySelector(".answers");
-const submit = document.querySelector(".submit");
-const play = document.querySelector(".play");
+const quizGame = document.querySelector(".quiz__game");
+const quizResult = document.querySelector(".quiz__result");
+const quizQuestion = document.querySelector(".quiz__question");
+const quizAnswersContainer = document.querySelector(".quiz__answers");
+const quizSubmit = document.querySelector(".quiz__submit");
+const quizPlayAgain = document.querySelector(".quiz__play-again");
 
 let questionIndex = 0;
 let correctCount = 0;
@@ -82,22 +82,20 @@ const playAgain = () => {
 	showQuestion(questionIndex);
 };
 
-play.addEventListener("click", () => {
-	gameScreen.style.display = "block";
-	resultScreen.style.display = "none";
+quizPlayAgain.addEventListener("click", () => {
+	quizGame.style.display = "block";
+	quizResult.style.display = "none";
 
 	playAgain();
 });
 
 const showResult = () => {
-	gameScreen.style.display = "none";
-	resultScreen.style.display = "block";
+	quizGame.style.display = "none";
+	quizResult.style.display = "block";
 
-	resultScreen.querySelector(".correct").textContent = `Correct Answers: ${correctCount}`;
-
-	resultScreen.querySelector(".wrong").textContent = `Wrong Answers: ${wrongCount}`;
-
-	resultScreen.querySelector(".score").textContent = `Score: ${(correctCount - wrongCount) * 10}`;
+	quizResult.querySelector(".correct").textContent = `Correct Answers: ${correctCount}`;
+	quizResult.querySelector(".wrong").textContent = `Wrong Answers: ${wrongCount}`;
+	quizResult.querySelector(".score").textContent = `Score: ${(correctCount - wrongCount) * 10}`;
 };
 
 const showQuestion = (questionNumber) => {
@@ -105,12 +103,12 @@ const showQuestion = (questionNumber) => {
 	if (questionIndex === data.length) return showResult();
 
 	selectedAnswer = null; // Для того, чтобы при обновлении вопроса мы не могли просто нажать кнопку "Submit" без выбранного ответа на странице
-	question.textContent = data[questionNumber].question;
+	quizQuestion.textContent = data[questionNumber].question;
 
 	// Используем следующую конструкцию для вставки нужного количества вариантов ответа, используя map(), и заканчиваем .join из-за того, что map() нам отдает массив, и выводиться он будет вместе с знаком ",", чтобы этого избежать пишем .join("")
-	answersContainer.innerHTML = data[questionNumber].answers.map((item, index) =>
+	quizAnswersContainer.innerHTML = data[questionNumber].answers.map((item, index) =>
 		`
-		<div class="answer">
+		<div class="quiz__answer">
 			<input name="answer" type="radio" id="${index}" value="${item.isCorrect}">
 			<label for="${index}">${item.answer}</label>
 		</div>
@@ -121,7 +119,7 @@ const showQuestion = (questionNumber) => {
 };
 
 const selectAnswer = () => {
-	answersContainer.querySelectorAll("input").forEach(item => {
+	quizAnswersContainer.querySelectorAll("input").forEach(item => {
 		item.addEventListener("click", (e) => {
 			selectedAnswer = e.target.value; // возвращает value input`a, в нашем случае true / false
 		});
@@ -129,7 +127,7 @@ const selectAnswer = () => {
 };
 
 const submitAnswer = () => {
-	submit.addEventListener("click", () => {
+	quizSubmit.addEventListener("click", () => {
 		if (selectedAnswer !== null) {
 			selectedAnswer === "true" ? correctCount++ : wrongCount++;
 			questionIndex++;

c8b16407e782f85f0de1b69c6f842242f6db0eb3
Ilya Kramarenko <il.kramarenko1@gmail.com>
2023-02-16 17:53:47 +0200

    updated vanillaQuiz js

--- vanillaQuiz/script.js
+++ vanillaQuiz/script.js
@@ -1,58 +1,58 @@
 const data = [
 	{
 		id: 1,
-		question: "Question 1",
+		question: "It`s a question 1",
 		answers: [
 			{
-				answer: "Answer 1",
+				answer: "It`s a true answer",
 				isCorrect: true
 			},
 			{
-				answer: "Answer 2",
+				answer: "What is it?",
 				isCorrect: false
 			},
 			{
-				answer: "Answer 3",
+				answer: "Where am I?",
 				isCorrect: false
 			},
 			{
-				answer: "Answer 4",
+				answer: "It`s a true answer (it`s a joke)",
 				isCorrect: false
 			},
 		],
 	},
 	{
 		id: 2,
-		question: "Question 2",
+		question: "It`s a question 2",
 		answers: [
 			{
-				answer: "2_Answer 1",
+				answer: "No-no, I'm not true",
 				isCorrect: false
 			},
 			{
-				answer: "2_Answer 2",
+				answer: "Hello! I am a true answer",
 				isCorrect: true
 			},
 			{
-				answer: "2_Answer 3",
+				answer: "What am I doing here?",
 				isCorrect: false
 			},
 		],
 	},
 	{
 		id: 3,
-		question: "Question 3",
+		question: "OMG O_o! It`s a question 3",
 		answers: [
 			{
-				answer: "3_Answer 1",
+				answer: "Not true",
 				isCorrect: false
 			},
 			{
-				answer: "3_Answer 2",
+				answer: "Not true",
 				isCorrect: false
 			},
 			{
-				answer: "3_Answer 3",
+				answer: "Please, choose me",
 				isCorrect: true
 			},
 		],
@@ -93,16 +93,21 @@ const showResult = () => {
 	quizGame.style.display = "none";
 	quizResult.style.display = "block";
 
+	total = Math.round(correctCount * 100 / data.length);
+
+	if (total >= 50) quizResult.querySelector("h1").textContent = "Congratulations!";
+	else quizResult.querySelector("h1").textContent = "Could be better!";
+
 	quizResult.querySelector(".correct").textContent = `Correct Answers: ${correctCount}`;
 	quizResult.querySelector(".wrong").textContent = `Wrong Answers: ${wrongCount}`;
-	quizResult.querySelector(".score").textContent = `Score: ${(correctCount - wrongCount) * 10}`;
+	quizResult.querySelector(".score").textContent = `Score: ${total}%`;
 };
 
 const showQuestion = (questionNumber) => {
 	// Запускаем функцию showResult если qIndex равен количеству всех вопросов (то есть вопрос является последним)
 	if (questionIndex === data.length) return showResult();
 
-	selectedAnswer = null; // Для того, чтобы при обновлении вопроса мы не могли просто нажать кнопку "Submit" без выбранного ответа на странице
+	selectedAnswer = null; // для того, чтобы при обновлении вопроса мы не могли просто нажать кнопку "Submit" без выбранного ответа на странице
 	quizQuestion.textContent = data[questionNumber].question;
 
 	// Используем следующую конструкцию для вставки нужного количества вариантов ответа, используя map(), и заканчиваем .join из-за того, что map() нам отдает массив, и выводиться он будет вместе с знаком ",", чтобы этого избежать пишем .join("")

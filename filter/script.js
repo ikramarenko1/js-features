e8aa2c0edfe758ab1d12ef9f7aec39277f76bdea
Ilya Kramarenko <il.kramarenko1@gmail.com>
2023-02-17 16:00:16 +0200

    cleanup

--- filter/script.js
+++ filter/script.js
@@ -4,55 +4,55 @@ const data = [
 		name: "Invicta Men's Pro Diver",
 		img: "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
 		price: 74,
-		cat: "Dress",
+		category: "Dress",
 	},
 	{
 		id: 2,
 		name: "Invicta Men's Pro Diver 2",
 		img: "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
 		price: 74,
-		cat: "Dress",
+		category: "Dress",
 	},
 	{
 		id: 3,
 		name: "Timex Men's Expedition Scout ",
 		img: "https://m.media-amazon.com/images/I/91WvnZ1g40L._AC_UY879_.jpg",
 		price: 40,
-		cat: "Sport",
+		category: "Sport",
 	},
 	{
 		id: 4,
 		name: "Breitling Superocean Heritage",
 		img: "https://m.media-amazon.com/images/I/61hGDiWBU8L._AC_UY879_.jpg",
 		price: 200,
-		cat: "Luxury",
+		category: "Luxury",
 	},
 	{
 		id: 5,
 		name: "Casio Classic Resin Strap ",
 		img: "https://m.media-amazon.com/images/I/51Nk5SEBARL._AC_UY879_.jpg",
 		price: 16,
-		cat: "Sport",
+		category: "Sport",
 	},
 	{
 		id: 6,
 		name: "Garmin Venu Smartwatch ",
 		img: "https://m.media-amazon.com/images/I/51kyjYuOZhL._AC_SL1000_.jpg",
 		price: 74,
-		cat: "Casual",
+		category: "Casual",
 	},
 ];
 
 const productsContainer = document.querySelector(".products");
 const searchInput = document.querySelector(".search");
-const categoriesContainer = document.querySelector(".cats");
-const priceRange = document.querySelector(".priceRange");
-const priceValue = document.querySelector(".priceValue");
+const categoriesContainer = document.querySelector(".categories");
+const priceRange = document.querySelector(".price__range");
+const priceValue = document.querySelector(".price__value");
 
 // Функция отображения предметов
 const displayProducts = (filteredProducts) => {
 	// Используем следующую конструкцию для вставки нужного количества товаров, используя map(), и заканчиваем .join из-за того, что map() нам отдает массив, и выводиться он будет вместе с знаком ",", чтобы этого избежать пишем .join("")
-	productsContainer.innerHTML = filteredProducts.map(product => 
+	productsContainer.innerHTML = filteredProducts.map(product =>
 		`
 		<div class="product">
 			<img src="${product.img}" alt="">
@@ -70,7 +70,7 @@ displayProducts(data);
 // keyup отображает значение клавиатуры всякий раз, когда вы отпускаете клавишу внутри <input> элемента
 searchInput.addEventListener("keyup", (e) => {
 	const value = e.target.value.toLowerCase();
-	
+
 	if (value) {
 		/*
 		/	Пример работы indexOf:
@@ -87,27 +87,27 @@ searchInput.addEventListener("keyup", (e) => {
 });
 
 const setCategories = () => {
-	const allCats = data.map((item) => item.cat);
+	const allCategories = data.map((item) => item.category);
 	const categories = [
 		"All",
 		// ... используется для того, чтобы к "All" присоединился массив из следующих объектов. Если не использовать ... в этом случае - будет массив состоять из двух объектов: "All" и второго массива
-		...allCats.filter((item, i) => {
-			return allCats.indexOf(item) === i;
+		...allCategories.filter((item, i) => {
+			return allCategories.indexOf(item) === i;
 		}),
 	];
-	
-	categoriesContainer.innerHTML = categories.map(cat =>
+
+	categoriesContainer.innerHTML = categories.map(category =>
 		`
-		<span class="cat">${cat}</span>
+		<span class="category">${category}</span>
 		`
 	).join("");
 
 	categoriesContainer.addEventListener("click", (e) => {
-		const selectedCategory = e.target.textContent; // вытягиваем категорию из <span class="cat"></span>
+		const selectedCategory = e.target.textContent; // вытягиваем категорию из <span class="category"></span>
 
-		selectedCategory === "All" 
-			? displayProducts(data) 
-			: displayProducts(data.filter((item) => item.cat === selectedCategory));
+		selectedCategory === "All"
+			? displayProducts(data)
+			: displayProducts(data.filter((item) => item.category === selectedCategory));
 	});
 };
 
